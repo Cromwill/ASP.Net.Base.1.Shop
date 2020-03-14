b45de0e0573a2543c769ee08a3d22624159b8502
Cromwill <33699699+Cromwill@users.noreply.github.com>
2020-03-14 11:47:35 +0300

    Create App

---
+++ ASP.Net.Base.1.Shop/ASP.Net.Base.1.Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.Net.Base._1.Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- ASPNet.Base1/ASPNet.Base1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNet.Base1.MvcApplication" Language="C#" %>
